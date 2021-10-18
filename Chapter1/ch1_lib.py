cd60619c1e9e913c553ea0ce6eda2977da99614e
eriks <e.suer@tu-berlin.de>
2021-10-17 21:28:33 -0700

    First draft of chapter 1 iypy and renaming

--- Chapter1/ch1_lib.py
+++ Chapter1/ch1_lib.py
@@ -133,7 +133,7 @@ def create_widgets_play_slider(fnc, minv, maxv, step=1, play_speed=200):
     widgets
     """
     slider = widgets.IntSlider(min=minv, max=maxv, step=step, continuous_update=True)
-    play = widgets.Play(min=minv, interval=play_speed)
+    play = widgets.Play(min=minv,max=maxv, step=step, interval=play_speed)
 
     slider.observe(fnc, 'value')
     widgets.jslink((play, 'value'), (slider, 'value'))
